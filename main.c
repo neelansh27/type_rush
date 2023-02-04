38c82ea4189233db17333ce3a706440afce8b49b
neelansh27 <neelanshsharma30@gmail.com>
2023-02-04 13:18:21 +0530

    Input screen is now working

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+
 int startup(int x, int y) {
   // window setup
   WINDOW *menu = newwin(10, x / 5, y / 2 - y / 4, x / 2 - x / 10);
@@ -47,7 +48,8 @@ void* timer() {
   WINDOW *Timer = newwin(3, 17, 2, (int)x / 2 + x / 4);
   box(Timer, 0, 0);
   for (int i=60; i>0; --i) {
-    mvwprintw(Timer, 1, 1, "Time Left: %ds", i);
+    curs_set(0);
+    i<10 ? mvwprintw(Timer, 1, 1, "Time Left: %ds ", i):mvwprintw(Timer, 1, 1, "Time Left: %ds ", i);
     wrefresh(Timer);
     sleep(1);
   }
@@ -76,6 +78,7 @@ int main() {
   WINDOW *win = newwin(7, x / 2, 5, (int)x / 2 - x / 4);
   refresh();
   box(win, 0, 0);
+  keypad(win , true );
   init_pair(1, COLOR_CYAN, COLOR_BLACK);
   attron(COLOR_PAIR(1));
   mvwprintw(win, 0, x / 4 - 5, "Sample Text");
@@ -85,10 +88,61 @@ int main() {
   //Using Multithreading to run timer in background
   pthread_t Time_thread;
   pthread_create(&Time_thread, NULL, timer, NULL);
-  sleep(2);
-  mvwprintw(win, 3, x/4-5, "Sample Text");
-  wrefresh(win);    
-  getch();
+  wrefresh(win);
+  //USER INPUT
+  int index=0,col=1,r=1,display_text;
+  char input[300];
+  while ((display_text=wgetch(win))!=27) {
+    curs_set(1);
+    if (col==(x/2-1)){
+      wmove(win, ++r, col=1);
+    }
+    switch (display_text) {
+      case '\n':
+        r++;
+        col=1;
+        wmove(win , r, col);
+        wrefresh(win);
+        input[index]='\n';
+        index++;
+        break;
+      case KEY_BACKSPACE:
+        if (col>1)
+          mvwprintw(win , r, --col, " \b");
+        wrefresh(win);
+        input[--index]='\0';
+        break;
+      case KEY_UP:
+        if (r>1)
+          wmove(win , --r, col);
+        wrefresh(win);
+        break;
+      case KEY_DOWN:
+        if (r<5)
+          wmove(win , ++r, col);
+        wrefresh(win);
+        break;
+      case KEY_END:
+        wmove(win, r, col=(x/2-2));
+        break;
+      case KEY_HOME:
+        wmove(win, r, col=1);
+        break;
+      case KEY_RIGHT:
+      case KEY_LEFT:
+      /* case KEY_D */
+        break;
+      default:
+        if (r<=5) {
+        input[index]=display_text;
+        mvwprintw(win , r, col, "%c",display_text);
+        wrefresh(win);
+        col++;
+        index++;
+        }
+      }
+  }
   endwin();
+  printf("%s",input);
   return 0;
 }
