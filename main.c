c75562a30bc02cf354c4976be5c492c612d35deb
neelansh27 <neelanshsharma30@gmail.com>
2023-02-05 14:16:03 +0530

    Added Separate text for each mode

--- main.c
+++ main.c
@@ -33,7 +33,10 @@ int main() {
   refresh();
   mode = startup(x, y);
   clear();
-  char dummy_text[]="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then ";
+  char* dummy_text[3]={
+  "! $%& /() =?* '<> #|;abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ ! $%& /() =?* '<> #|; ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent !$%& /() =?* '<> #|; {} abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !$%& /() =?* '<> #|;{} em quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum r!$%& /() =?* '<> #|;{} abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV bWXYZ ! $%& /() =?* '<> #|; {} abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ ! $%& /() =?* '<> #| abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ ! $%& /() =?* '<> #| {} abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !%& /() =? '<> #|; {} abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ  $%& /() =?* '<> #|; abc def ghi jkl mno pqrs tuv",
+  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec ",
+  "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn't listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then "};
   //Main Screen
   WINDOW *win = newwin(22, x / 2, 5, x / 2 - x / 4);
   refresh();
@@ -58,11 +61,11 @@ int main() {
   pthread_create(&Time_thread, NULL, timer, NULL);
   int row=1,col=1;
   wattron(win, COLOR_PAIR(2));
-  for (int i = 0; i < strlen(dummy_text);i++) {
+  for (int i = 0; i < strlen(dummy_text[mode]);i++) {
       if(col>x/2-2){
         wmove(win, ++row, col=1);
       }
-      mvwprintw(win, row, col, "%c",dummy_text[i]);
+      mvwprintw(win, row, col, "%c",dummy_text[mode][i]);
       col++;
   }
   wattroff(win, COLOR_PAIR(2));
@@ -81,7 +84,7 @@ int main() {
         if (col>1){
           wattron(win, COLOR_PAIR(2));
           col--;
-          mvwprintw(win , row,col, "%c\b",dummy_text[(row-1)*(x/2-2)+col-1]);
+          mvwprintw(win , row,col, "%c\b",dummy_text[mode][(row-1)*(x/2-2)+col-1]); //Formula for replacing with orignal text
           wattroff(win, COLOR_PAIR(3));
           input[--index]='\0';
         }
@@ -117,7 +120,7 @@ int main() {
   float g_wpm,n_wpm,acc;
   char_len=strlen(input);
   total_time= (total_time<=0) ? 1: 1-(total_time/60);//Time will become -1 after last iteration
-  err=errors(dummy_text, input);
+  err=errors(dummy_text[mode], input);
   n_wpm=net_wpm(char_len,err, total_time);
   g_wpm=gross_wpm(char_len,total_time);
   acc=accuracy(n_wpm, g_wpm);
